8bf970c9bcfd5e33a04169fd00cc3749475d1985
Radoslaw Mejer <radmen@baethon.com>
2020-04-29 11:46:12 +0200

    Fix linter errors

--- tests/seeds/users.js
+++ tests/seeds/users.js
@@ -4,7 +4,7 @@ module.exports = {
 
     return knex('users').insert([
       { first_name: 'Jon', last_name: 'Snow', active: true },
-      { first_name: 'Sansa', last_name: 'Stark', active: false },
+      { first_name: 'Sansa', last_name: 'Stark', active: false }
     ])
   }
 }
