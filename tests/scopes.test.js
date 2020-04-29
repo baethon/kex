77418a0d7356c5b32f6168d73c9ace6571dcfcdb
Radoslaw Mejer <radmen@baethon.com>
2020-04-29 19:34:35 +0200

    Ignore selected scopes

--- tests/scopes.test.js
+++ tests/scopes.test.js
@@ -103,6 +103,70 @@ test('global scopes | combine with other filters', t => {
   equalQueries(t, expected, actual)
 })
 
-test.todo('global scopes | ignore selected')
-test.todo('global scopes | disable on update')
-test.todo('global scopes | disable on insert')
+test('global scopes | ignore selected', t => {
+  const { knex } = t.context
+  const User = createKex(t).createModel('User', {
+    globalScopes: {
+      active: scopes.activeUser,
+      age: (qb) => qb.where('age', '>=', 18)
+    }
+  })
+
+  const expected = knex('users')
+    .where(qb => qb.where('age', '>=', 18))
+
+  equalQueries(t, expected, User.query().withoutGlobalScopes(['active']))
+  equalQueries(t, expected, User.query().withoutGlobalScope('active'))
+})
+
+test('global scopes | ignore all scopes', t => {
+  const { knex } = t.context
+  const User = createKex(t).createModel('User', {
+    globalScopes: {
+      active: scopes.activeUser,
+      age: (qb) => qb.where('age', '>=', 18)
+    }
+  })
+
+  equalQueries(t, knex('users'), User.query().withoutGlobalScopes())
+})
+
+test('global scopes | include in update', t => {
+  const { knex } = t.context
+  const User = createKex(t).createModel('User', {
+    globalScopes: {
+      active: scopes.activeUser
+    }
+  })
+
+  const expected = knex('users')
+    .where('id', 1)
+    .where(scopes.activeUser)
+    .update({ name: 'Jon' })
+
+  const actual = User.query()
+    .where('id', 1)
+    .update({ name: 'Jon' })
+
+  equalQueries(t, expected, actual)
+})
+
+test.only('global scopes | include in delete', t => {
+  const { knex } = t.context
+  const User = createKex(t).createModel('User', {
+    globalScopes: {
+      active: scopes.activeUser
+    }
+  })
+
+  const expected = knex('users')
+    .where('id', 1)
+    .where(scopes.activeUser)
+    .delete()
+
+  const actual = User.query()
+    .where('id', 1)
+    .delete()
+
+  equalQueries(t, expected, actual)
+})
