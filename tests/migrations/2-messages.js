f0e2162c2f18afbfca1a05c8429e4f5519fa097d
Radoslaw Mejer <radmen@baethon.com>
2020-05-04 17:37:33 +0200

    Extend migrations & seeds

--- tests/migrations/2-messages.js
+++ tests/migrations/2-messages.js
@@ -4,10 +4,13 @@ module.exports = {
       .createTable('messages', (table) => {
         table.increments('id')
         table.integer('user_id').unsigned().notNullable()
+        table.integer('to_user').unsigned().notNullable()
+        table.string('from_username')
         table.string('text')
         table.datetime('deleted_at').nullable()
 
         table.foreign('user_id').references('users.id')
+        table.foreign('to_user').references('users.id')
       })
   },
 
--- tests/seeds/2-messages.js
+++ tests/seeds/2-messages.js
@@ -13,18 +13,33 @@ module.exports = {
     return knex('messages').insert([
       {
         user_id: jon.id,
+        to_user: sansa.id,
+        from_username: jon.username,
         text: 'Message #1'
       },
       {
         user_id: jon.id,
+        to_user: sansa.id,
+        from_username: jon.username,
         text: 'Message #2'
       },
       {
+        user_id: jon.id,
+        to_user: sansa.id,
+        from_username: jon.username,
+        text: 'Message.deleted #1',
+        deleted_at: new Date()
+      },
+      {
         user_id: sansa.id,
+        to_user: jon.id,
+        from_username: sansa.username,
         text: 'Message #3'
       },
       {
         user_id: sansa.id,
+        to_user: jon.id,
+        from_username: sansa.username,
         text: 'Message #4'
       }
     ])
