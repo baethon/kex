307c5359208acfcc607b786e0bca2cab0bff35af
Radoslaw Mejer <radmen@baethon.com>
2020-05-04 16:49:39 +0200

    Add new utils functions: mapTo, mapToMany, identity, prop

--- src/utils.js
+++ src/utils.js
@@ -11,4 +11,37 @@ const omit = (object, props) => props.reduce(
   { ...object }
 )
 
-module.exports = { omit }
+/**
+ * Map results of query to DataLoader-friendly format
+ *
+ * @param {String[]} keys
+ * @param {Function} keyFn the function used to get the key from result row
+ * @return {Object[]}
+ */
+const mapTo = (keys, keyFn) => (rows) => {
+  const group = new Map(keys.map(key => ([key, null])))
+  rows.forEach(row => group.set(keyFn(row), row))
+  return Array.from(group.values())
+}
+
+/**
+ * Map results of query to DataLoader-friendly format.
+ * Group them by the key.
+ *
+ * @param {String[]} keys
+ * @param {Function} keyFn the function used to get the key from result row
+ * @return {Array.<Object[]>}
+ */
+const mapToMany = (keys, keyFn) => (rows) => {
+  const group = new Map(keys.map(key => ([key, []])))
+  rows.forEach(row => (group.get(keyFn(row)) || []).push(row))
+  return Array.from(group.values())
+}
+
+module.exports = {
+  omit,
+  mapTo,
+  mapToMany,
+  indentity: value => value,
+  prop: name => item => item[name]
+}
