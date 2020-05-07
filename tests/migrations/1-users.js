5cfa14d928e16132d920f10d524cbc517ad64327
Radoslaw Mejer <radmen@baethon.com>
2020-05-07 22:07:33 +0200

    Migrations: add unique constraint for users.username

--- tests/migrations/1-users.js
+++ tests/migrations/1-users.js
@@ -3,7 +3,7 @@ module.exports = {
     return knex.schema
       .createTable('users', (table) => {
         table.increments('id')
-        table.string('username')
+        table.string('username').unique()
         table.string('first_name')
         table.string('last_name')
         table.boolean('active').default(false)
